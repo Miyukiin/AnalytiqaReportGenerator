e50ba7ff4c3a189b1860f8e4a41341bbbaeb2fee
Miyukiin <jr.concha99@gmail.com>
2024-12-07 03:14:06 +0800

    Added alternative encoding and additional read_csv options to reduce failures at reading csv because of encoding errors.

--- backend-Django/ReportGeneratorApp/views.py
+++ backend-Django/ReportGeneratorApp/views.py
@@ -8,7 +8,6 @@ from django.http import Http404, HttpRequest, HttpResponse, HttpResponseBadReque
 from django.middleware.csrf import get_token
 import pandas as pd
 from .models import Visitors
-from .utils import get_summary_data, get_csv_preview_data
 from django.core.exceptions import ObjectDoesNotExist
 from functools import wraps
 from django.core.files.base import ContentFile
@@ -21,6 +20,35 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Create your views here.
+def get_csv_preview_data(csv_file_of_uuid_path: str) -> dict | JsonResponse:
+    TYPE_MAPPING = {
+            "object": "string",
+            "int64": "number",
+            "float64": "number",
+            "bool": "boolean",
+    }
+    data = {} 
+    headers = {}   
+    
+    csv_file_path = os.path.join(settings.MEDIA_ROOT, csv_file_of_uuid_path)
+    with open(csv_file_path, encoding="ISO-8859-1") as csv_file:
+        logger.info("Reading CSV")
+        df:pd.DataFrame = pd.read_csv(csv_file, encoding="ISO-8859-1", encoding_errors='ignore', skip_blank_lines=True, on_bad_lines="skip")
+        logger.info("Finished Reading CSV")
+        
+        # Extract column headers and dtypes
+        for column in df.columns:
+            dtype = str(df[column].dtype)
+            ts_type = TYPE_MAPPING.get(dtype, "any")  # Default to "any" if no mapping is found
+            headers[column] = ts_type
+            
+        data["headers_types"] = headers
+        
+        # Include CSV rows as an array of dictionaries
+        df = df.where(pd.notnull(df), "") # Replace Nan, None null things with empty string for mapping
+        data["data"] = df.to_dict(orient="records")  # Converts each row into a dictionary
+
+        return data
 
 @api_view(['GET'])
 def sample_api(request:HttpRequest):
@@ -48,6 +76,7 @@ def validate_uuid(func):
 def get_table_preview_data(request:HttpRequest, query_object: Visitors):
     if request.method == "GET":
         try:
+            logger.info(str(query_object.orig_csv_file))
             preview_data:dict = get_csv_preview_data(str(query_object.orig_csv_file))
             return JsonResponse(preview_data)
 
@@ -70,6 +99,58 @@ def get_cleaned_table_preview_data(request:HttpRequest, query_object: Visitors):
 @api_view(['GET'])
 @validate_uuid
 def get_summary_statistics(request:HttpRequest, query_object: Visitors):
+    def get_summary_data(csv_file_of_uuid_path: str) -> dict | JsonResponse:
+        csv_file_path = os.path.join(settings.MEDIA_ROOT, csv_file_of_uuid_path)
+        
+        with open(csv_file_path, encoding="ISO-8859-1") as csv_file:
+            df:pd.DataFrame = pd.read_csv(csv_file, encoding="ISO-8859-1", encoding_errors='ignore', skip_blank_lines=True, on_bad_lines="skip")
+            
+            if df.empty:
+                return JsonResponse({"error": "The uploaded CSV file is empty"}, status=400)
+            
+            data = {"name": os.path.basename(csv_file_path)}
+            
+            
+            # Get row and column count of csv file
+            data["row_count"] = df.shape[0]
+            data["column_count"] = df.shape[1]
+            
+            # Get Duplicate and Unique Count
+            duplicate_count = int(df.duplicated().sum())
+            data["duplicate_count"] = duplicate_count
+            data["unique_count"] = len(df) - duplicate_count
+            
+            # Get number of blank cells
+            data["total_number_blank_cells"] = int(df.isna().sum().sum())
+            
+            # Numeric Columns Data
+            # Select numeric columns only
+            numeric_columns = df.select_dtypes(include="number")
+            numeric_columns_stats = {}
+            
+            if not numeric_columns.empty:
+                # Calculate statistical measures for each numeric column, if exists
+                for column_name in numeric_columns.columns:
+                    column = numeric_columns[column_name]
+                    temp_dict = {
+                        "Min": int(column.min()),
+                        "Max": int(column.max()),
+                        "Standard_Deviation": int(column.std()),
+                        "Variance": int(column.var()),
+                        "Mean": int(column.mean()),
+                        "Median": int(column.median()),
+                        "Mode": int(column.mode().iloc[0]) if not column.mode().empty else None,  # Handle empty mode
+                        "Quartiles": {
+                            "Q1": int(column.quantile(0.25)),
+                            "Q2": int(column.quantile(0.5)),
+                            "Q3": int(column.quantile(0.75)),
+                        },
+                    }
+                    numeric_columns_stats[column_name] = temp_dict
+                data["numeric_columns_stats"] = numeric_columns_stats
+            
+            return data
+
     if request.method == "GET":
         try:
             summary_data:dict = get_summary_data(str(query_object.orig_csv_file))
