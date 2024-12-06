2a694b3a32d8e7e07697d900307307dbaf38095b
Miyukiin <jr.concha99@gmail.com>
2024-12-07 00:09:26 +0800

    Added nullguard in SummaryChanges.tsx.

--- frontend-NextJs/src/components/summaryChanges.tsx
+++ frontend-NextJs/src/components/summaryChanges.tsx
@@ -29,21 +29,21 @@ const SummaryChanges: React.FC<{ summaryChangesData: SummaryChangesData }> = ({ 
 
     <div>
         <Typography variant="body1" sx={{ color: 'grey.800', marginBottom: 0, lineHeight: '1.8' }}>
-            {rows_removed!== null ? <span>Step 1: We removed <b>{rows_removed}</b> duplicate rows.</span> : null}
+            {rows_removed && rows_removed!== null ? <span>Step 1: We removed <b>{rows_removed}</b> duplicate rows.</span> : null}
         </Typography>
         <Typography variant="body1" sx={{ color: 'grey.800', marginBottom: 0, lineHeight: '1.8' }}>
-            {missing_values_replaced !== null ? <span>Step 2: We replaced <b>{missing_values_replaced}</b> missing values.</span> : null}
+            {missing_values_replaced && missing_values_replaced !== null ? <span>Step 2: We replaced <b>{missing_values_replaced}</b> missing values.</span> : null}
         </Typography>
         <Typography variant="body1" sx={{ color: 'grey.800', marginBottom: 0, lineHeight: '1.8' }}>
-            {Object.keys(column_changes).length > 0 ? <span>Step 3: We identified all columns we changed below, with a count of values or cells modified.</span> : null}
+            {column_changes && Object.keys(column_changes).length > 0 ? <span>Step 3: We identified all columns we changed below, with a count of values or cells modified.</span> : null}
         </Typography>
-        {Object.entries(column_changes).map(([column, changes]) => (
+        {column_changes && Object.entries(column_changes).map(([column, changes]) => (
             <Typography key={column} variant="body1" sx={{ color: 'grey.700', marginBottom: 0, marginLeft: 2 }}>
                 {<span> - {column}: <b>{changes}</b> changed values</span>}
             </Typography>
         ))}
         <Typography variant="body1" sx={{ color: 'grey.800', marginTop: 1, lineHeight: '1.8' }}>
-            {removed_columns.length > 0 ? <span>Lastly, we also <b>{removed_columns.length}</b> removed columns that did not contain any data: <b>{removed_columns.join(', ')}</b></span> : null}
+            {removed_columns && removed_columns.length > 0 ? <span>Lastly, we also <b>{removed_columns.length}</b> removed columns that did not contain any data: <b>{removed_columns.join(', ')}</b></span> : null}
         </Typography>
     </div>
 
